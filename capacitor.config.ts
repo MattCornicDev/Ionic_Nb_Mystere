92f48113d5e9eb0a211c7b4ae8bb592312cad59c
Rarib <59053047+MattCornicDev@users.noreply.github.com>
2021-10-28 09:57:16 +0200

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'nbMystere',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
