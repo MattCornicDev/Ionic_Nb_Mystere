2bfd56feffcc2b3b98309684b1810c7874b4b313
Rarib <59053047+MattCornicDev@users.noreply.github.com>
2021-10-28 12:02:48 +0200

    création page règle

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -11,6 +11,10 @@ const routes: Routes = [
     redirectTo: 'home',
     pathMatch: 'full'
   },
+  {
+    path: 'regle',
+    loadChildren: () => import('./regle/regle.module').then( m => m.ReglePageModule)
+  },
 ];
 
 @NgModule({
---
+++ src/app/regle/regle.page.spec.ts
@@ -0,0 +1,24 @@
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
+import { IonicModule } from '@ionic/angular';
+
+import { ReglePage } from './regle.page';
+
+describe('ReglePage', () => {
+  let component: ReglePage;
+  let fixture: ComponentFixture<ReglePage>;
+
+  beforeEach(waitForAsync(() => {
+    TestBed.configureTestingModule({
+      declarations: [ ReglePage ],
+      imports: [IonicModule.forRoot()]
+    }).compileComponents();
+
+    fixture = TestBed.createComponent(ReglePage);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  }));
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
